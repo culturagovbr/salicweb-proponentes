15379d9622950a0bed48f878c2318798d6d52114
Matheus Batista <matheusbattista@hotmail.com>
2017-09-05 19:08:02 -0300

    Configures docker-compose to set app environment

--- salicweb/settings.py
+++ salicweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&tr_hgpt^0%75x1^(q3fbcy6g)+atnt+hqy)_sq5@ov@gn0&m1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','localhost' ]
 
 
 # Application definition
@@ -84,8 +84,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'salicweb-proponentes',
         'USER':'postgres',
-        'PASSWORD':'123456',
-        'HOST':'localhost',
+        'PASSWORD':'root',
+        'HOST':'db',
         'PORT':'5432',
     }
 }
